c1d082c44e51cb3699f64d60f11dfe2e712e076b
Cedric Lueke <cedric.lueke@gmail.com>
2017-03-13 23:24:55 +0100

    Add Login
    Taken from: https://devdactic.com/login-ionic-2/

--- GreenCityApp/src/app/app.module.ts
+++ GreenCityApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { MyApp } from './app.component';
 import { CommunityPage } from '../pages/community/community';
 import { EinstellungenPage } from '../pages/einstellungen/einstellungen';
 import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 import { NachrichtenPage } from '../pages/nachrichten/nachrichten';
 import { SpotsPage } from '../pages/spots/spots';
 import { StaedtePage } from '../pages/staedte/staedte';
 import { UeberPage } from '../pages/ueber/ueber';
 import { WissenPage } from '../pages/wissen/wissen';
+import { AuthService } from '../providers/auth-service'
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { WissenPage } from '../pages/wissen/wissen';
     CommunityPage,
     EinstellungenPage,
     LoginPage,
+    RegisterPage,
     NachrichtenPage,
     SpotsPage,
     StaedtePage,
@@ -31,12 +34,13 @@ import { WissenPage } from '../pages/wissen/wissen';
     CommunityPage,
     EinstellungenPage,
     LoginPage,
+    RegisterPage,
     NachrichtenPage,
     SpotsPage,
     StaedtePage,
     UeberPage,
     WissenPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, AuthService]
 })
 export class AppModule {}
--- GreenCityApp/src/pages/login/login.ts
+++ GreenCityApp/src/pages/login/login.ts
@@ -1,22 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/*
-  Generated class for the Login page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { RegisterPage } from '../register/register';
+import { SpotsPage } from '../spots/spots';
+ 
+ /*
+ * Simple LoginPage Controller from https://devdactic.com/login-ionic-2/
+ */
+ 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  loading: Loading;
+  registerCredentials = {email: '', password: ''};
+ 
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController, private loadingCtrl: LoadingController) {}
+ 
+  public createAccount() {
+    this.nav.push(RegisterPage);
   }
-
+ 
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {
+        setTimeout(() => {
+        this.loading.dismiss();
+        this.nav.setRoot(SpotsPage)
+        });
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+    error => {
+      this.showError(error);
+    });
+  }
+ 
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+ 
+  showError(text) {
+    setTimeout(() => {
+      this.loading.dismiss();
+    });
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
 }
